d0fd3815a77fcaa5668f1e5d9212d6ee0bbb7b4c
Usama <usama18053lf@gmail.com>
2025-07-17 16:54:59 +0500

    New commponents are added

---
+++ RaatingApp/src/app/main-app/main-app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-app',
+  imports: [],
+  templateUrl: './main-app.html',
+  styleUrl: './main-app.css'
+})
+export class MainApp {
+
+}
